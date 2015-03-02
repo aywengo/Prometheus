61c80a56bf7d84999d5d15c155d86397fe21a1bd
Roman Melnyk <aywengo@gmail.com>
2015-03-02 20:10:33 +0100

    reorder files in folder

--- src/Prometheus_week5/Main.java
+++ src/Prometheus_week5/Main.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        File inputFile = new File(String.format("%s\\src\\Prometheus_week5\\%s",
+        File inputFile = new File(String.format("%s\\src\\Prometheus_week5\\inputs\\%s",
                 System.getProperty("user.dir"), "input_16_10000.txt"));
         ArrayList<String> stringArrayList = Prometheus_week4.Helper.readFile(inputFile.toPath());
 
